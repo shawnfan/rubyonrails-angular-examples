88084e5c60eb5b7e3f2e0cc8ad37183ef4c71d6d
shawnfan <zhangfan.shawn@gmail.com>
2015-10-02 00:06:37 -0400

    added angular login example

---
+++ angular-login-validation/controller/mainCtrl.js
@@ -0,0 +1,39 @@
+angular.module('challenge',['ngMessages'])
+.controller('MainCtrl', ['$scope',function($scope){
+	$scope.email = "";
+	$scope.username = "";
+	$scope.password = "";
+	$scope.register = function() {
+		$scope.formValidate = false;
+		$scope.submitAttempt = true;
+
+		$scope.formValidate = jQuery.isEmptyObject($scope.registerForm.$error);
+
+		if ($scope.formValidate) {
+			$scope.submitAttempt = false;
+			console.log("Email: " + $scope.email + "\nUsername: " + $scope.username + "\nPassword: " + $scope.password);
+		} else {
+			alert("Please enter correct information for registeration! Thanks");
+		}
+	};
+	
+}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
